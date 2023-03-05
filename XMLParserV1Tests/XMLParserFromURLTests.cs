0cbf20daf9296b487ce13d50f1f05ef2ac6f85d0
hpharis258 <60554302+hpharis258@users.noreply.github.com>
2023-03-05 16:16:31 +0000

    Tests

---
+++ XMLParserV1Tests/XMLParserFromURLTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XMLParserV1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLParserV1.Tests
+{
+    [TestClass()]
+    public class XMLParserFromURLTests
+    {
+        [TestMethod()]
+        public void GetAllDataTestDataType()
+        {
+            // Arrange
+            XMLParserFromURL XMLParser = new XMLParserFromURL("https://www.theyworkforyou.com/pwdata/scrapedxml/regmem/regmem2021-12-13.xml");
+            XMLParser.GetRecordCount = 0;
+            // Act
+            List<MemberOfParliament> List = XMLParser.GetAllData();
+            // Assert
+            Assert.IsInstanceOfType(List, typeof(List<MemberOfParliament>));
+        }
+
+        [TestMethod()]
+        public void GetAllDataTestIsNotNull()
+        {
+            // Arrange
+            XMLParserFromURL XMLParser = new XMLParserFromURL("https://www.theyworkforyou.com/pwdata/scrapedxml/regmem/regmem2021-12-13.xml");
+            XMLParser.GetRecordCount = 0;
+            // Act
+            List<MemberOfParliament> List = XMLParser.GetAllData();
+            // Assert
+            Assert.IsNotNull(List);
+        }
+
+        [TestMethod()]
+        public void GetAllDataTestDidReturnAllMembers()
+        {
+            // Arrange
+            XMLParserFromURL XMLParser = new XMLParserFromURL("https://www.theyworkforyou.com/pwdata/scrapedxml/regmem/regmem2021-12-13.xml");
+            XMLParser.GetRecordCount = 0;
+            // Act
+            List<MemberOfParliament> List = XMLParser.GetAllData();
+            // Assert
+            Assert.IsTrue(List.Count == 647);
+        }
+    }
+}
